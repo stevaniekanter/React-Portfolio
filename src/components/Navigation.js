83a5023ec7ffafc81ff5b09a2eb5550be058da01
Stevanie Kanter <stevaniek@yahoo.com>
2021-11-20 16:05:18 -0500

    adjusted nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,20 +3,20 @@ import React from 'react';
 function Navigation({ handlePageChange }) {
     return (
         
-            <nav class="navigation center-align col 12 grey lighten-2">
-                <button id="small-text" class="navbtn btn col 4 light-blue darken-3" onClick={() => handlePageChange('AboutMe')}>
+            <nav class="navigation right-align col 12 purple lighten-2">
+                <button class="navbtn btn col 4 purple darken-3" id="text" onClick={() => handlePageChange('AboutMe')}>
                     About Me
                 </button>
 
-                <button id="small-text" class="navbtn btn col 4 light-blue darken-3" onClick={() => handlePageChange('Portfolio')}>
+                <button class="navbtn btn col 4 purple darken-3" id="text" onClick={() => handlePageChange('Portfolio')}>
                     Portfolio
                 </button>
 
-                <button id="small-text" class="navbtn btn col 4 light-blue darken-3" onClick={() => handlePageChange('Contact')}>
+                <button class="navbtn btn col 4 purple darken-3" id="text" onClick={() => handlePageChange('Contact')}>
                     Contact
                 </button>
 
-                <a id="small-text" class="navbtn btn col 4 light-blue darken-3" hhref="./assets/resume/resume.pdf" target="_blank" rel="noreferrer noopener" download>
+                <a class="navbtn btn col 4 purple darken-3" id="text" href="assets/resume/resume.pdf" target="_blank" rel="noreferrer noopener" download>
                     Resume
                 </a>
             </nav>
