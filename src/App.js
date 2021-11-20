3196b7a73c82a39554d7448da39022733a72c26f
Stevanie Kanter <stevaniek@yahoo.com>
2021-11-20 16:03:56 -0500

    adjusted content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Main />
       <Footer />
     </div>
